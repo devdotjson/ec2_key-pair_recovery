43e949356f5a22bca8ceed21b9d98b4f8d3f695d
Rene Audain <rene.audain.ctr@teambespin.us>
2024-12-30 18:23:36 -0600

    removed outputs to resolve error

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "public_ip" {
-  value = aws_instance.test.public_ip
+# output "public_ip" {
+#   value = aws_instance.test.public_ip
   
-}
+# }
 
-output "ssh_address" {
-  value = "ssh -i '${local_file.working_private_key.filename}' ubuntu@ec2${aws_instance.test.public_ip}.${var.region}.compute.amazonaws.com"
-}
+# output "ssh_address" {
+#   value = "ssh -i '${local_file.working_private_key.filename}' ubuntu@ec2${aws_instance.test.public_ip}.${var.region}.compute.amazonaws.com"
+# }
